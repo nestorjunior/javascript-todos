53c02db9a66b7b90745e9760b20e9926dd591fd3
Nestor Júnior <nestorjunior@MacBook-de-Nestor.local>
2019-04-07 00:39:55 -0300

    function renderiza todos

--- todos.js
+++ todos.js
@@ -1,3 +1,21 @@
 var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
+
+var todos = [
+	'Fazer café',
+	'Estudar Javascript',
+	'Ler a Bíblia'
+];
+
+function renderTodos(){
+	for (todo of todos) {
+		var todoElement = document.createElement('li');
+		var todoText = document.createTextNode(todo);
+
+		todoElement.appendChild(todoText);
+		listElement.appendChild(todoElement);
+	}
+}
+
+renderTodos();
