e3dc5f09e09a1723be8cedf61c1fa626e52f2b21
Nestor Júnior <nestorjunior@MacBook-de-Nestor.local>
2019-04-07 01:01:35 -0300

    create function remove todo

--- todos.js
+++ todos.js
@@ -9,13 +9,42 @@ var todos = [
 ];
 
 function renderTodos(){
+
+	listElement.innerHTML = '';
+
 	for (todo of todos) {
 		var todoElement = document.createElement('li');
 		var todoText = document.createTextNode(todo);
 
+		var linkElement = document.createElement('a');
+		var linkText = document.createTextNode(' Excluir');
+
+		linkElement.appendChild(linkText);
+		linkElement.setAttribute('href', '#');
+
+		var pos = todos.indexOf(todo);
+		linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+
 		todoElement.appendChild(todoText);
+		todoElement.appendChild(linkElement);
 		listElement.appendChild(todoElement);
 	}
 }
 
 renderTodos();
+
+function addTodo(){
+	var todoText = inputElement.value;
+
+	todos.push(todoText);
+	inputElement.value = '';
+	renderTodos();
+}
+
+buttonElement.onclick = addTodo;
+
+function deleteTodo(pos){
+	console.log('chamou delete');
+	todos.splice(0, 1);
+	renderTodos();
+}
